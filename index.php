7e91a58b1168b1df07dfd34df376f864ffc66434
Сергей <sergey.ivanov840@gmail.com>
2022-05-23 01:19:13 +0700

    работа с условиями

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+			<input class="checkbox__input visually-hidden show_completed<?php if($show_complete_tasks==1): ?>checked<?php endif; ?>" type="checkbox">
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -90,10 +90,24 @@ $show_complete_tasks = rand(0, 1);
                             <a class="download-link" href="#">Home.psd</a>
                         </td>
 
-                        <td class="task__date"></td>
-                    </tr>
+			<td class="task__date"></td>
+		    </tr>  
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                </table>
+                    <?php 
+                    if($show_complete_tasks == 1): ?>
+                   <tr class = "tasks_item task task--completed">
+                   <td class = "task_select">
+                   <label class = "checkbox task_checkbox">
+                   <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                   <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                   </label>
+                   </td>
+                   <td class="task__date">10.10.2019</td>
+                   <td class="task__controls"></td>
+		   </tr>
+                   
+                   <?php endif; ?>
+                 </table>
             </main>
         </div>
     </div>
