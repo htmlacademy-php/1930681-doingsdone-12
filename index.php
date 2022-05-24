9a3eda21e7ea9c0f76c53fc5cb30b2d78fa592bd
Сергей <sergey.ivanov840@gmail.com>
2022-05-24 16:19:51 +0700

    Подправил код условий

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-			<input class="checkbox__input visually-hidden show_completed<?php if($show_complete_tasks==1): ?>checked<?php endif; ?>" type="checkbox">
+			<input class="checkbox__input visually-hidden show_completed<?php if ($show_complete_tasks): ?>checked<?php endif; ?>" type="checkbox">
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -93,19 +93,17 @@ $show_complete_tasks = rand(0, 1);
 			<td class="task__date"></td>
 		    </tr>  
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?php 
-                    if($show_complete_tasks == 1): ?>
+                    <?php if($show_complete_tasks == 1) : ?>
                    <tr class = "tasks_item task task--completed">
                    <td class = "task_select">
-                   <label class = "checkbox task_checkbox">
-                   <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                   <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                   </label>
+                       <label class = "checkbox task_checkbox">
+                           <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                           <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                       </label>
                    </td>
                    <td class="task__date">10.10.2019</td>
                    <td class="task__controls"></td>
 		   </tr>
-                   
                    <?php endif; ?>
                  </table>
             </main>
